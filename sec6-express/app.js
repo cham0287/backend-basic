fe4fba536993da97473f48705b9fbdeaca997a39
cham0287 <crtmt97@gmail.com>
2024-01-03 23:36:08 +0900

    Feat: express로 html 서빙하기

--- sec6-express/app.js
+++ sec6-express/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
+app.set('port', 3001);
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/', (req, res) => {
   res.send('hello express');
 });
 
-app.listen(3001, () => {
+app.get('/about', (req, res) => {
+  res.send('hello express');
+});
+
+app.listen(app.get('port'), () => {
   console.log('execute express server');
 });
